4655ce92d132a993cfd88b68f8d2fbe8b4b94c40
surajk843 <164765494+surajk843@users.noreply.github.com>
2026-06-23 16:10:09 +0530

    chapter 1

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1> Hello world from vite| suraj Kanojiya</h1>
+  );
+}
+
+export default App
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,9 @@
+import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
